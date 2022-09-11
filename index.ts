63e24149bf6404e9fa5596aa6e65ef22f75e5932
susanev <susan.ra.evans@gmail.com>
2022-09-11 11:27:20 -0700

    did more stuff
    Signed-off-by: susanev <susan.ra.evans@gmail.com>

--- index.ts
+++ index.ts
@@ -3,85 +3,31 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as fs from "fs";
 import * as mime from "mime";
+import { CdnWebsite } from "./cdn-website";
+import * as checkly from "@checkly/pulumi";
 const staticWebsiteDirectory = "website";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2(
-    "bucketV2",
-    {
-      tags: {
-        Name: "My bucket",
-      },
-    }
-  );
-  
-const bucketAcl = new aws.s3.BucketAclV2("bAcl", {
-    bucket: bucket.id,
-    acl: aws.s3.PublicReadAcl,
+const website = new CdnWebsite("your-startup", {});
+export const websiteUrl = website.url;
+
+new checkly.Check("index-page", {
+  activated: true,
+  frequency: 10,
+  type: "BROWSER",
+  locations: ["eu-west-2"],
+  script: websiteUrl.apply((url) =>
+    fs
+      .readFileSync("checkly-embed.js")
+      .toString("utf8")
+      .replace("{{websiteUrl}}", url)
+  ),
 });
 
-fs.readdirSync(staticWebsiteDirectory).forEach((file) => {
-    const filePath = `${staticWebsiteDirectory}/${file}`;
-    const fileContent = fs.readFileSync(filePath).toString();
+import { Swag } from "./swag-provider";
 
-    new aws.s3.BucketObject(file, {
-        bucket: bucket.id,
-        source: new pulumi.asset.FileAsset(filePath),
-        contentType: mime.getType(filePath) || undefined,
-        acl: aws.s3.PublicReadAcl,
-    });
+const swag = new Swag("your-startup", {
+  name: "susan",
+  email: "susan.ra.evans@gmail.com",
+  address: "32707 ne 195th st duvall wa 98019 united states",
+  size: "M",
 });
-
-const s3OriginId = "myS3Origin";
-
-const cloudfrontDistribution = new aws.cloudfront.Distribution(
-  "s3Distribution",
-  {
-    origins: [
-      {
-        domainName: bucket.bucketRegionalDomainName,
-        originId: s3OriginId,
-      },
-    ],
-    enabled: true,
-    isIpv6Enabled: true,
-    comment: "Some comment",
-    defaultRootObject: "index.html",
-    defaultCacheBehavior: {
-      allowedMethods: [
-        "DELETE",
-        "GET",
-        "HEAD",
-        "OPTIONS",
-        "PATCH",
-        "POST",
-        "PUT",
-      ],
-      cachedMethods: ["GET", "HEAD"],
-      targetOriginId: s3OriginId,
-      forwardedValues: {
-        queryString: false,
-        cookies: {
-          forward: "none",
-        },
-      },
-      viewerProtocolPolicy: "allow-all",
-      minTtl: 0,
-      defaultTtl: 3600,
-      maxTtl: 86400,
-    },
-    priceClass: "PriceClass_200",
-    restrictions: {
-      geoRestriction: {
-        restrictionType: "whitelist",
-        locations: ["US", "CA", "GB", "DE"],
-      },
-    },
-    viewerCertificate: {
-      cloudfrontDefaultCertificate: true,
-    },
-  }
-);
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
